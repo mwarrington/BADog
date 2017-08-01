18737d34040584d68d6654c26c4bc8b564fc0d1f
mwarrington <mason.arrington@gmail.com>
2017-08-01 15:44:41 -0500

    Start of dialog system

---
+++ gravity/Assets/scripts/DialogSystem/DialogLine.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogLine : MonoBehaviour
+{
+    public string Speaker;
+    public string LineText;
+    public int Index;
+}
---
+++ gravity/Assets/scripts/DialogSystem/DialogUIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogUIController : MonoBehaviour
+{
+    private Image _textBoxImage;
+    private Text _textBoxText;
+
+    void Start()
+    {
+        _textBoxImage = GameObject.Find("DialogCanvas/DialogBox").GetComponent<Image>();
+        _textBoxText = GameObject.Find("DialogCanvas/DialogBox/DialogText").GetComponent<Text>();
+    }
+}
