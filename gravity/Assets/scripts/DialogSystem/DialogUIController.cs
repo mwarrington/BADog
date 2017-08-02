a225e8da042d8d38ad341cfcf2c7e55e3b4bc37f
mwarrington <mason.arrington@gmail.com>
2017-08-02 16:01:50 -0500

    litte more for DO

--- gravity/Assets/scripts/DialogSystem/DialogUIController.cs
+++ gravity/Assets/scripts/DialogSystem/DialogUIController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class DialogUIController : MonoBehaviour
 {
+    private Button _dialogOption1,
+                   _dialogOption2;
     private Image _textBoxImage;
     private Text _textBoxText;
     private Vector3 _textBoxToNPCPos;
@@ -14,5 +16,8 @@ public class DialogUIController : MonoBehaviour
         _textBoxImage = GameObject.Find("DialogToNPCPoint/DialogBox").GetComponent<Image>();
         _textBoxText = GameObject.Find("DialogToNPCPoint/DialogBox/DialogText").GetComponent<Text>();
         _textBoxToNPCPos = GameObject.Find("DialogToNPCPoint").transform.position;
+
+        _dialogOption1 = GameObject.Find("DialogOptions1").GetComponent<Button>();
+        _dialogOption2 = GameObject.Find("DialogOptions2").GetComponent<Button>();
     }
 }
