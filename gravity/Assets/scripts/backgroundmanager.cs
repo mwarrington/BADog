1bf0f6c00db6dd7a4c6cdc320c38247e4c88a8c0
mtruong68 <marytruong2020@u.northwestern.edu>
2017-08-11 12:21:51 -0500

    New stage pieces

--- gravity/Assets/scripts/backgroundmanager.cs
+++ gravity/Assets/scripts/backgroundmanager.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class BackgroundManager : MonoBehaviour {
 
     public GameObject[] BackgroundPrefabs;
+    private float _timer = 34f;
 
     void DestroyAdd(Collider2D col)
     {
         Destroy(col.transform.gameObject);
 
-        Instantiate(
-            BackgroundPrefabs[UnityEngine.Random.Range(0, BackgroundPrefabs.Length)],
-            new Vector3(19, 0, 0),
-            Quaternion.identity
-        );
+        if (_timer > 10f)
+        {
+            Instantiate(
+                BackgroundPrefabs[UnityEngine.Random.Range(0, BackgroundPrefabs.Length)],
+                new Vector3(19, 0, 0),
+                Quaternion.identity
+            );
+        }
+    }
+
+    void Update()
+    {
+            _timer -= Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D col)
